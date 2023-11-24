e20165962a3d838225d0a0f7d1c235203df12e6d
Nathan <892819284@qq.com>
2023-11-24 11:12:31 +0800

    2023-11-24 daily question

---
+++ leetcode-daily-questions/20231124/en/Solution1561.java
@@ -0,0 +1,26 @@
+package en;
+
+import java.util.Arrays;
+
+/**
+ * <a href="https://leetcode.com/problems/maximum-number-of-coins-you-can-get/?envType=daily-question&envId=2023-11-24">...</a>
+ * <p>
+ * Medium
+ */
+public class Solution1561 {
+
+    /**
+     * 贪心 + 双指针
+     * <p>
+     * 每次取走第二多的堆
+     */
+    public int maxCoins(int[] piles) {
+        Arrays.sort(piles);
+        int ans = 0, r = piles.length - 2, n = piles.length / 3;
+        for (int l = 0; l < n; l++) {
+            ans += piles[r];
+            r -= 2;
+        }
+        return ans;
+    }
+}
---
+++ leetcode-daily-questions/20231124/zh/Solution2824.java
@@ -0,0 +1,41 @@
+package zh;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * <a href="https://leetcode.cn/problems/count-pairs-whose-sum-is-less-than-target/description/?envType=daily-question&envId=2023-11-24">...</a>
+ * <p>
+ * Easy
+ */
+public class Solution2824 {
+    /**
+     * 暴力解法，时间复杂度 O(n^2)
+     */
+    public int countPairs(List<Integer> nums, int target) {
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums.get(i) + nums.get(j) < target) ans++;
+            }
+        }
+        return ans;
+    }
+
+
+    /**
+     * 排序 + 双指针
+     * <p>
+     * 时间复杂度：O(n log n)
+     */
+    public int countPairs1(List<Integer> nums, int target) {
+        int ans = 0, n = nums.size();
+        nums.sort(Integer::compareTo);
+        Collections.sort(nums);
+        for (int l = 0, r = n - 1; l < r; l++) {
+            while (r >= 0 && nums.get(l) + nums.get(r) >= target) r--;
+            if (l < r) ans += r - l;
+        }
+        return ans;
+    }
+}
