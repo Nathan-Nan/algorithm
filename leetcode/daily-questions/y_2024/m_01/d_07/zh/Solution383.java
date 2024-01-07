2720b2d0e2e138f5c84ce86921f96f910cf99aff
Nathan <892819284@qq.com>
2024-01-07 18:26:13 +0800

    daily question 2024-01-07

---
+++ leetcode/daily-questions/y_2024/m_01/d_07/zh/Solution383.java
@@ -0,0 +1,26 @@
+package y_2024.m_01.d_07.zh;
+
+/**
+ * <a href="https://leetcode.cn/problems/ransom-note/description/?envType=daily-question&envId=2024-01-07">...</a>
+ */
+public class Solution383 {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] rChars = getChars(ransomNote);
+        int[] mChars = getChars(magazine);
+        for (int i = 0; i < 26; i++) {
+            if (rChars[i] > mChars[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int[] getChars(String s) {
+        int[] chars = new int[26];
+        for (char c : s.toCharArray()) {
+            chars[c - 'a']++;
+        }
+        return chars;
+    }
+
+}
