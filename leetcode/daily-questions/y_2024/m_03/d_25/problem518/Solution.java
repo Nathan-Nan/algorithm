19a254bc7d575ff8ddcb524b95a3305f1df51402
Nathan <892819284@qq.com>
2024-03-27 22:21:52 +0800

    daily problem 2024-03-25

---
+++ leetcode/daily-questions/y_2024/m_03/d_25/problem518/Solution.java
@@ -0,0 +1,17 @@
+package y_2024.m_03.d_25.problem518;
+
+/**
+ * <a href="https://leetcode.cn/problems/coin-change-ii/description/?envType=daily-question&envId=2024-03-25">...</a>
+ */
+public class Solution {
+    public int change(int amount, int[] coins) {
+        int[] f = new int[amount + 1];
+        f[0] = 1;
+        for (int x : coins) {
+            for (int c = x; c <= amount; c++) {
+                f[c] += f[c - x];
+            }
+        }
+        return f[amount];
+    }
+}
