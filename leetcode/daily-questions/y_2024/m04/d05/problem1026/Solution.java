3054f33df8f72fbd1cba335ab40ccd14f8a46689
Nathan <892819284@qq.com>
2024-04-05 16:20:40 +0800

    daily problem 2024-04-05

---
+++ leetcode/daily-questions/y_2024/m04/d05/problem1026/Solution.java
@@ -0,0 +1,9 @@
+package y_2024.m04.d05.problem1026;
+
+/**
+ * <a href="https://leetcode.cn/problems/maximum-difference-between-node-and-ancestor/description/?envType=daily-question&envId=2024-04-05">...</a>
+ *
+ * {@link y_2024.m01.d_11.en.Solution1026}
+ */
+public class Solution {
+}
